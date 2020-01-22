c985f3419835a50688afe811d86213d9cdeca498
Jonny Graybill <jon.a.graybill@gmail.com>
2020-01-21 18:28:07 -0800

    initial files and magic eight ball widget

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # widgets
+
+A repo where I can keep the skeleton code for all the little widgets and cool functions I build here and there.
---
+++ magic-eight-ball.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let userName = '';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!') ;
+
+const userQuestion = 'Will I get lucky tonight?';
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy, try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Don\'t count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes'
+  default:
+    eightBall = 'I\'m not sure why you\'re asking me.';
+}
+
+let playEightBall = function(userName, userQuestion) {
+  console.log(`${userName} wants to know: ${userQuestion.toLowerCase()} Answer: ${eightBall}`)
+}
+
+playEightBall('Jonny', 'Do I love programming?');
